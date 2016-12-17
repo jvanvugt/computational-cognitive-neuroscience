4a2da146e0032d81dd72690923a5f802902c3af0
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2016-12-17 02:15:00 +0100

    Fix job script; Add theano flags

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -p gpu
 #SBATCH -N 1
-#SBATCH -t 70:00:00
+#SBATCH -t 100:00:00
 module load cuda
 module load cudnn
-module load python/3.4.1
-python train_model.py
+module load python/2.7.9
+THEANO_FLAGS='mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1' srun python train_model.py
