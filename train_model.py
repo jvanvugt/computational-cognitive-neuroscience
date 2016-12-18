d4126c4dc7cb96172e10881f76287f372c679009
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2016-12-18 02:02:36 +0100

    Sequence to sequence learning

--- train_model.py
+++ train_model.py
@@ -5,9 +5,9 @@ https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
 
 from __future__ import print_function
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, LSTM, Dropout
+from keras.layers.wrappers import TimeDistributed
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, LambdaCallback
-from keras.layers import LSTM, Dropout
 from keras.optimizers import RMSprop
 import numpy as np
 import sys
@@ -30,27 +30,27 @@ indices_char = dict((i, c) for i, c in enumerate(chars))
 maxlen = 100
 step = 11
 sentences = []
-next_chars = []
+offset_sentences = []
 for i in range(0, len(text) - maxlen, step):
     sentences.append(text[i: i + maxlen])
-    next_chars.append(text[i + maxlen])
+    offset_sentences.append(text[i+1: i+maxlen+1])
 print('nb sequences:', len(sentences))
 
 print('Vectorization...')
 X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
-y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
-for i, sentence in enumerate(sentences):
-    for t, char in enumerate(sentence):
+y = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+for i, (sentence, off_sentence) in enumerate(zip(sentences, offset_sentences)):
+    for t, (char, next_char) in enumerate(zip(sentence, off_sentence)):
         X[i, t, char_indices[char]] = 1
-    y[i, char_indices[next_chars[i]]] = 1
+        y[i, t, char_indices[next_char]] = 1
 
 print('Build model...')
 model = Sequential()
-model.add(LSTM(512, input_shape=(maxlen, len(chars)), return_sequences=True))
+model.add(LSTM(128, input_shape=(maxlen, len(chars)), return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(512))
 model.add(Dropout(0.2))
-model.add(Dense(len(chars)))
+model.add(TimeDistributed(Dense(len(chars))))
 model.add(Activation('softmax'))
 
 optimizer = RMSprop(lr=2e-3)
@@ -79,9 +79,8 @@ def generate_text():
         print()
         print('----- diversity:', diversity)
 
-        generated = ''
         sentence = text[start_index: start_index + maxlen]
-        generated += sentence
+        generated = sentence
         print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
 
@@ -90,11 +89,10 @@ def generate_text():
             for t, char in enumerate(sentence):
                 x[0, t, char_indices[char]] = 1.
 
-            preds = model.predict(x, verbose=0)[0]
+            preds = model.predict(x, verbose=0)[0, -1, :]
             next_index = sample(preds, diversity)
             next_char = indices_char[next_index]
 
-            generated += next_char
             sentence = sentence[1:] + next_char
 
             sys.stdout.write(next_char)
@@ -111,5 +109,5 @@ if resume:
 # train the model, output generated text after each iteration
 history = model.fit(X, y, batch_size=128, nb_epoch=50, validation_split=0.1,
                     callbacks=[checkpointer, lr_schedule, sampler])
-with open('history.pkl') as file:
+with open('history.pkl', 'w') as file:
     pickle.dump(history, file)
