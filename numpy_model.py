6b41deec9eaa1b08f2f4a85b2f4aee3b90deaeb4
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2016-12-19 01:26:23 +0100

    Add second LSTM layer to numpy model

--- numpy_model.py
+++ numpy_model.py
@@ -72,18 +72,18 @@ def create_lstm_matrices(filepath, layer):
     """
     prefix = '{0}/{0}_'.format(layer)
     with h5py.File(filepath) as f:
-        Wx = np.hstack((f[prefix + 'W_i:0'][:],
-                        f[prefix + 'W_f:0'][:],
-                        f[prefix + 'W_o:0'][:],
-                        f[prefix + 'W_c:0'][:]))
-        Wh = np.hstack((f[prefix + 'U_i:0'][:],
-                        f[prefix + 'U_f:0'][:],
-                        f[prefix + 'U_o:0'][:],
-                        f[prefix + 'U_c:0'][:]))
-        b =  np.hstack((f[prefix + 'b_i:0'][:],
-                        f[prefix + 'b_f:0'][:],
-                        f[prefix + 'b_o:0'][:],
-                        f[prefix + 'b_c:0'][:]))
+        Wx = np.hstack((f[prefix + 'W_i'][:],
+                        f[prefix + 'W_f'][:],
+                        f[prefix + 'W_o'][:],
+                        f[prefix + 'W_c'][:]))
+        Wh = np.hstack((f[prefix + 'U_i'][:],
+                        f[prefix + 'U_f'][:],
+                        f[prefix + 'U_o'][:],
+                        f[prefix + 'U_c'][:]))
+        b =  np.hstack((f[prefix + 'b_i'][:],
+                        f[prefix + 'b_f'][:],
+                        f[prefix + 'b_o'][:],
+                        f[prefix + 'b_c'][:]))
     return Wx, Wh, b
 
 def create_dense_matrices(filepath, layer):
@@ -93,8 +93,8 @@ def create_dense_matrices(filepath, layer):
     """
     prefix = layer + '/dense_1_'
     with h5py.File(filepath) as f:
-        W = f[prefix + 'W:0'][:]
-        b = f[prefix + 'b:0'][:]
+        W = f[prefix + 'W'][:]
+        b = f[prefix + 'b'][:]
         return W, b
 
 
@@ -104,10 +104,11 @@ def load_data(filepath):
     look-up tables
     """
     with open(filepath) as file:
-        text = file.read()[:2000000]
+        text = file.read()
     print('corpus length:', len(text))
 
-    chars = sorted(list(set(text)))
+    # The '\r' disappears on windows machines, so we manually add it back
+    chars = sorted(list(set(text)) + ['\r'])
     print('total chars:', len(chars))
     char_indices = dict((c, i) for i, c in enumerate(chars))
     indices_char = dict((i, c) for i, c in enumerate(chars))
@@ -135,29 +136,35 @@ def generate_text(initial_char='d', timesteps=400, temperature=1., save_cells=Tr
     The result is printed.
     """
     # Load the weights
-    Wx, Wh, b = create_lstm_matrices('weights.hdf5', 'lstm_1')
+    Wx_1, Wh_1, b_1 = create_lstm_matrices('weights.hdf5', 'lstm_1')
+    Wx_2, Wh_2, b_2 = create_lstm_matrices('weights.hdf5', 'lstm_2')
     Wd, bd = create_dense_matrices('weights.hdf5', 'timedistributed_1')
     _, char_indices, indices_char = load_data('D:/Data/python.txt')
 
     # Set up the initial state
     x = np.zeros((timesteps+1, len(char_indices)))
     x[0, char_indices[initial_char]] = 1
-    H = Wh.shape[0]
-    h = np.zeros((timesteps+1, H))
-    c = np.zeros_like(h)
-    buffer = ''
+    H1 = Wh_1.shape[0] # Number of units in the first LSTM layer
+    H2 = Wh_2.shape[0] # Number of units in the second LSTM layer
+    h1 = np.zeros((timesteps+1, H1))
+    c1 = np.zeros_like(h1)
+    h2 = np.zeros((timesteps+1, H2))
+    c2 = np.zeros_like(h2)
+    buffer = initial_char
 
     # Forward passes through the model
     for t in range(timesteps):
-        h[t+1, :], c[t+1, :] = lstm_step_forward(x[t, :].reshape(1, -1), h[t, :], c[t, :], Wx, Wh, b)
-        logprobs = np.dot(h[t+1], Wd) + bd
+        h1[t+1, :], c1[t+1, :] = lstm_step_forward(x[t, :].reshape(1, -1), h1[t, :], c1[t, :], Wx_1, Wh_1, b_1)
+        h2[t+1, :], c2[t+1, :] = lstm_step_forward(h1[t+1, :].reshape(1, -1), h2[t, :], c2[t, :], Wx_2, Wh_2, b_2)
+        logprobs = np.dot(h2[t+1], Wd) + bd
         probs = softmax(logprobs.reshape(1, -1))[0]
         next_index = sample(probs, temperature)
         buffer += indices_char[next_index]
         x[t+1, next_index] = 1
     
     if save_cells:
-        np.savetxt('cell_states.csv', c, delimiter=',', fmt='%.4f')
+        all_cells = np.hstack((c1, c2))
+        np.savetxt('cell_states.csv', all_cells, delimiter=',', fmt='%.4f')
     
     with open('generated.txt', 'w') as file:
         file.write(buffer)
